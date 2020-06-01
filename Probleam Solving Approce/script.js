a78d0fb62a98966a892e54c8cc4716df949a1fe8
Abdul Motin <motin.mmk.jr@gmail.com>
2020-06-01 15:18:45 +0600

    test-1

--- Probleam Solving Approce/script.js
+++ Probleam Solving Approce/script.js
@@ -1,3 +1,31 @@
 function charCount(str) {
+    // make this an object to return 
+    var result = {};
+    // Run loop for every char get
+    for (var i = 0; i < str.length; i++) {
+        var char = str[i];
+        if (result[char] > 0) {
+            result[char]++;
+        } else {
+            result[char] = 1;
+        };
 
+    }
+    return result;
+} // now its work for niche 
+
+
+function charCountP(str) {
+    var obj = {};
+    for (var char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            if (obj[char] > 0) {
+                obj[char]++;
+            } else {
+                obj[char] = 1;
+            };
+        }
+    }
+    return obj;
 }
