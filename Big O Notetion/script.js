1320be1d0ca011b49b78ebfa451aa134d7c9e404
Abdul Motin <motin.mmk.jr@gmail.com>
2020-06-01 11:46:40 +0600

    JavaSacript

--- Big O Notetion/script.js
+++ Big O Notetion/script.js
@@ -33,3 +33,16 @@ var t1 = performance.now();
 addUpTo(10000000000)
 var t2 = performance.now();
 console.log((t1 - t2) / 1000)
+
+
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+var t1 = performance.now();
+addUpTo(10000000000)
+var t2 = performance.now();
+console.log((t1 - t2) / 1000)
